7fec5fd9293185198aaa2b64b635781dcb06ddbc
Ismaili <bismai01@KN200993.local>
2017-06-21 12:44:34 +0200

    cards 2.0

--- public/js/cards.js
+++ public/js/cards.js
@@ -1,17 +1,18 @@
 Howler.mobileAutoEnable = true;
 
+console.log('init')
+
 var sound = new Howl({
-  src: ['sound/plop.mp3']
+	src: ['sound/plop.mp3']
 });
 
 var socket = io();
 
 socket.on('comment',function(_data){
-	sound.play();
 
-	$('.cards_container').empty();
-	$card = $('<div>').attr('class',"card");
-	$card.delay(_data.sec*1000).fadeOut(300);
+	$card = $('<li>').attr('class',"card");
+
+
 
 	$img = $('<img>').attr('src',_data.pic);
 	$name = $('<h1>').html(_data.name);
@@ -21,7 +22,30 @@ socket.on('comment',function(_data){
 	$card.append($name);
 	$card.append($message);
 
+	$card.on('swipe',function(){
+
+		$(this).fadeOut(500,function(){
+			$(this).remove();
+		});
+	});
+
 	$('.cards_container').append($card);
 });
 
 
+
+
+socket.on('timer',function(_data){
+	if(_data.bool){
+		console.log('timer');
+		var now = new Date();
+		now.setMinutes(now.getMinutes() + 20);
+		$('.timer').countdown(now, function(event) {
+			$(this).html(event.strftime('%M:%S'));
+		});	
+	}else{
+		$('.timer').countdown(new Date(), function(event) {
+			$(this).html(event.strftime('%M:%S'));
+		});			}
+
+	})
